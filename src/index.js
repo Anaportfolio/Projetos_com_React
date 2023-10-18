c968e5f68f11702c0f2420bcf076074c02b7318e
Ana Beatriz <biamaiaribeiro@gmail.com>
2023-10-18 12:06:23 -0300

    calculator

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Calculadora</h1>);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
