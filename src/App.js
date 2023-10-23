bc391ae59cff78ba15bb27d8e225981d639e0dfc
Ana Beatriz <biamaiaribeiro@gmail.com>
2023-10-23 10:37:57 -0300

    calculator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Calculator from "./Main/Calculator";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Calculadora</h1>
+      <Calculator />
     </div>
   );
 }
